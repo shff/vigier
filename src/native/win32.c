26f9c49c256a68028324c6c89a10b790bb5d7ce3
Silvio Henrique Ferreira <shferreira@me.com>
2021-01-05 01:15:55 +0100

    Add DirectX G-Buffer

--- src/native/win32.c
+++ src/native/win32.c
@@ -24,12 +24,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                                CW_USEDEFAULT, NULL, NULL, instance, NULL);
   ShowWindow(window, SW_SHOWNORMAL);
 
-  // Initialize DX11
-  IDXGISwapChain *swapchain = NULL;
-  ID3D11Device *dev = NULL;
-  ID3D11DeviceContext *context = NULL;
-  ID3D10Texture2D *renderTexture = NULL;
-  ID3D11RenderTargetView *backbuffer = NULL;
+  // Create Swap-Chain
   DXGI_SWAP_CHAIN_DESC desc = {
       .BufferCount = 1,
       .BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM,
@@ -38,17 +33,59 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
       .SampleDesc.Count = 4,
       .Windowed = TRUE,
   };
-  D3D11_VIEWPORT viewport = {
-      .TopLeftX = 0, .TopLeftY = 0, .Width = 800, .Height = 600};
+  IDXGISwapChain *swapchain = NULL;
+  ID3D11Device *dev = NULL;
+  ID3D11DeviceContext *context = NULL;
   D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, NULL,
                                 0, D3D11_SDK_VERSION, &desc, &swapchain, &dev,
                                 NULL, &context);
-  swapchain->lpVtbl->GetBuffer(swapchain, 0, &IID_ID3D11Texture2D,
-                               &renderTexture);
-  dev->lpVtbl->CreateRenderTargetView(dev, (ID3D11Resource *)renderTexture,
-                                      NULL, &backbuffer);
-  context->lpVtbl->OMSetRenderTargets(context, 1, &backbuffer, NULL);
-  context->lpVtbl->RSSetViewports(context, 1, &viewport);
+
+  // Create G-Buffer
+  D3D11_TEXTURE2D_DESC gBufferTexDesc = {
+      .Width = 800,
+      .Height = 600,
+      .MipLevels = 1,
+      .ArraySize = 1,
+      .Format = DXGI_FORMAT_R32G32B32A32_FLOAT,
+      .SampleDesc.Count = 1,
+      .BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE,
+  };
+  D3D11_RENDER_TARGET_VIEW_DESC gBufferDesc = {
+      .Format = DXGI_FORMAT_R32G32B32A32_FLOAT,
+      .ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D,
+  };
+  ID3D11Texture2D *gBufferTex = NULL;
+  ID3D11RenderTargetView *gBuffer = NULL;
+  dev->lpVtbl->CreateTexture2D(dev, &gBufferTexDesc, NULL, &gBufferTex);
+  dev->lpVtbl->CreateRenderTargetView(dev, (ID3D11Resource *)gBufferTex,
+                                      &gBufferDesc, &gBuffer);
+
+  // Create Z-Buffer
+  D3D11_TEXTURE2D_DESC zBufferTexDesc = {
+      .Width = 800,
+      .Height = 600,
+      .MipLevels = 1,
+      .ArraySize = 1,
+      .Format = DXGI_FORMAT_D24_UNORM_S8_UINT,
+      .SampleDesc.Count = 1,
+      .BindFlags = D3D11_BIND_DEPTH_STENCIL,
+  };
+  const D3D11_DEPTH_STENCIL_VIEW_DESC zBufferDesc = {
+      .Format = DXGI_FORMAT_D24_UNORM_S8_UINT,
+      .ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D,
+  };
+  ID3D11Texture2D *zBufferTex = NULL;
+  ID3D11RenderTargetView *zBuffer = NULL;
+  dev->lpVtbl->CreateTexture2D(dev, &zBufferTexDesc, NULL, &zBufferTex);
+  dev->lpVtbl->CreateRenderTargetView(dev, (ID3D11Resource *)zBufferTex,
+                                      &zBufferDesc, &zBuffer);
+
+  // Create the Backbuffer
+  ID3D11Texture2D *bufferTex = NULL;
+  ID3D11RenderTargetView *buffer = NULL;
+  swapchain->lpVtbl->GetBuffer(swapchain, 0, &IID_ID3D11Texture2D, &bufferTex);
+  dev->lpVtbl->CreateRenderTargetView(dev, (ID3D11Resource *)bufferTex, NULL,
+                                      &buffer);
 
   // Start the Timer
   long long timerResolution;
@@ -93,15 +130,29 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     {
     }
 
-    // Render
-    float color[4] = {0.0f, 0.2f, 0.4f, 1.0f};
-    context->lpVtbl->ClearRenderTargetView(context, backbuffer, color);
+    D3D11_VIEWPORT viewport = {0, 0, 800, 600, 1, 1000};
+    float blankColor[4] = {0.0f, 0.2f, 0.4f, 1.0f};
+
+    // Geometry Pass
+    context->lpVtbl->OMSetRenderTargets(context, 1, &gBuffer, &zBuffer);
+    context->lpVtbl->RSSetViewports(context, 1, &viewport);
+    context->lpVtbl->ClearRenderTargetView(context, gBuffer, blankColor);
+    context->lpVtbl->ClearDepthStencilView(context, zBuffer, D3D11_CLEAR_DEPTH,
+                                           1.0f, 0);
+
+    // Final Pass
+    context->lpVtbl->OMSetRenderTargets(context, 1, &buffer, &zBuffer);
+    context->lpVtbl->RSSetViewports(context, 1, &viewport);
     swapchain->lpVtbl->Present(swapchain, 0, 0);
   }
 
   swapchain->lpVtbl->Release(swapchain);
-  backbuffer->lpVtbl->Release(backbuffer);
-  renderTexture->lpVtbl->Release(renderTexture);
+  gBuffer->lpVtbl->Release(gBuffer);
+  gBufferTex->lpVtbl->Release(gBufferTex);
+  zBuffer->lpVtbl->Release(zBuffer);
+  zBufferTex->lpVtbl->Release(zBufferTex);
+  buffer->lpVtbl->Release(buffer);
+  bufferTex->lpVtbl->Release(bufferTex);
   dev->lpVtbl->Release(dev);
   context->lpVtbl->Release(context);
 
