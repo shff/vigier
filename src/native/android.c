03811c8d65ba74dbda9cd3fba3f85cd6637ce263
Silvio Henrique Ferreira <shferreira@me.com>
2021-01-16 19:18:22 +0100

    Add Android G-Buffer

--- src/native/android.c
+++ src/native/android.c
@@ -4,34 +4,73 @@
 
 EGLDisplay display;
 EGLSurface surface;
-EGLint w = 0, h = 0;
+unsigned int gbuffer;
 
 static void engine_handle_cmd(struct android_app *app, int32_t cmd)
 {
   if (cmd == APP_CMD_INIT_WINDOW)
   {
-    EGLint numConfigs, format;
-    EGLConfig config;
-    EGLContext context;
+    // Initialize Display
+    display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    eglInitialize(display, 0, 0);
+
+    // Set Format
     const EGLint attribs[] = {EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
                               EGL_BLUE_SIZE,    8,
                               EGL_GREEN_SIZE,   8,
                               EGL_RED_SIZE,     8,
                               EGL_NONE};
-
-    display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    eglInitialize(display, 0, 0);
+    EGLConfig config;
+    EGLint numConfigs, format;
     eglChooseConfig(display, attribs, &config, 1, &numConfigs);
     eglGetConfigAttrib(display, config, EGL_NATIVE_VISUAL_ID, &format);
     ANativeWindow_setBuffersGeometry(app->window, 0, 0, format);
     surface = eglCreateWindowSurface(display, config, app->window, NULL);
-    context = eglCreateContext(display, config, NULL, NULL);
-    eglMakeCurrent(display, surface, surface, context);
-    eglQuerySurface(display, surface, EGL_WIDTH, &w);
-    eglQuerySurface(display, surface, EGL_HEIGHT, &h);
 
+    // Initialize OpenGL
+    EGLContext context = eglCreateContext(display, config, NULL, NULL);
+    eglMakeCurrent(display, surface, surface, context);
+
+    // Get Surface Size
+    EGLint width = 0, height = 0;
+    eglQuerySurface(display, surface, EGL_WIDTH, &width);
+    eglQuerySurface(display, surface, EGL_HEIGHT, &height);
+
+    // Create G-Buffer
+    unsigned int backbuffer;
+    glGenTextures(1, &backbuffer);
+    glBindTexture(GL_TEXTURE_2D, backbuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_FLOAT,
+                 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+
+    // Create Z-Buffer
+    unsigned int depthbuffer;
+    glGenTextures(1, &depthbuffer);
+    glBindTexture(GL_TEXTURE_2D, depthbuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0,
+                 GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+
+    // Create Framebuffer
+    glGenFramebuffers(1, &gbuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, gbuffer);
+    glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, backbuffer, 0);
+    glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthbuffer, 0);
+    glDrawBuffers(2, (GLenum[]){GL_COLOR_ATTACHMENT0, GL_DEPTH_ATTACHMENT});
+
+    // OpenGL Configuration
     glEnable(GL_CULL_FACE);
     glEnable(GL_DEPTH_TEST);
+    glClearColor(0.9, 0.9, 0.9, 1);
   }
 }
 
@@ -73,8 +112,10 @@ void android_main(struct android_app *app)
     }
 
     // Renderer
-    glClearColor(0.9, 0.9, 0.9, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glBindFramebuffer(GL_FRAMEBUFFER, gbuffer);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     eglSwapBuffers(display, surface);
   }
 }
